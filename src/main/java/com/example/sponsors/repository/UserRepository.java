d08df89dfae7cc7a008a90499687320bc415fd12
Ângelo Fávero <angelo-favero@hotmail.com>
2024-12-10 17:32:36 -0300

    Configuração do Repositório do User

--- src/main/java/com/example/sponsors/repository/UserRepository.java
+++ src/main/java/com/example/sponsors/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.sponsors.repository;
 
-public interface UserRepository {
+import com.example.sponsors.model.Sponsors;
+import com.example.sponsors.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
